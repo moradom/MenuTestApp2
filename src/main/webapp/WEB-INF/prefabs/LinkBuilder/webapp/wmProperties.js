91c58f6d4fc49b5e8a7638ffe557ca6aada6c220
Marco Morado <marco.morado@preito.com>
2017-10-11 13:53:49 +0000

    Initial Push

---
+++ src/main/webapp/WEB-INF/prefabs/LinkBuilder/webapp/wmProperties.js
@@ -0,0 +1,13 @@
+var _WM_APP_PROPERTIES = {
+  "activeTheme" : "default",
+  "dateFormat" : "",
+  "defaultLanguage" : "en",
+  "displayName" : "LinkBuilder",
+  "homePage" : "Main",
+  "name" : "LinkBuilder",
+  "platformType" : "DEFAULT",
+  "supportedLanguages" : "en",
+  "timeFormat" : "",
+  "type" : "PREFAB",
+  "version" : "1.03"
+};
---
+++ src/main/webapp/wmProperties.js
@@ -0,0 +1,14 @@
+var _WM_APP_PROPERTIES = {
+  "activeTheme" : "material",
+  "dateFormat" : "",
+  "defaultLanguage" : "en",
+  "displayName" : "MenuTestApp2",
+  "homePage" : "Main",
+  "name" : "MenuTestApp2",
+  "platformType" : "WEB",
+  "securityEnabled" : "true",
+  "supportedLanguages" : "en",
+  "timeFormat" : "",
+  "type" : "APPLICATION",
+  "version" : "1.0"
+};
